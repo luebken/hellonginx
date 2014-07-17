d276127c1b9b68a09db8a1edc01226d647734ffe
Matthias Lübken <matthias@dockzero.io>
2014-07-17 15:05:15 +0200

    using injected port

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sed -i "s/##NGINX_UPSTREAM_URL##/${WEATHER_PORT_1337_TCP_ADDR}:1337/g" /etc/nginx.conf
+sed -i "s/##NGINX_UPSTREAM_URL##/${WEATHER_PORT_1337_TCP_ADDR}/g" /etc/nginx.conf
+sed -i "s/##NGINX_UPSTREAM_PORT##/${WEATHER_PORT_1337_TCP_PORT}/g" /etc/nginx.conf
 nginx
